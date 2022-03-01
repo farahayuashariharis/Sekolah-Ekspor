9f1f84aadab665ac3f3f030041cb1b3e9aeab366
farahayuashariharis <19102096@ittelkom-pwt.ac.id>
2022-03-01 05:43:24 -0800

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html leng="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width-device-width, initial-scale-1.0">
+    <title>Belajar BE</title>
+</head>
+<body>
+    <h1> Hello </h1>
+    <?php
+        $var = "nama saya ayuni";
+    ?>
+    <?php echo "".$var; ?>
+</body>
+</html>
